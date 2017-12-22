ea6db0ccfba499aeb30f68a652e705e1376a3d51
Gabriel Schade <re035148@ITAW10N07304>
2017-12-22 17:10:18 -0200

    Commit para o exemplo com JsonTypeProvider

---
+++ JsonTypeProviderSample/JsonTypeProviderSample/JsonTypeProviderSample/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open FSharp.Data
+
+[<Literal>]
+let url = "https://api.github.com/users/gabrielschade"
+type PerfilGitHub = JsonProvider<url>
+[<EntryPoint>]
+let main argv =
+    let perfil = PerfilGitHub.Load url
+    printfn "%s com %i seguidores" perfil.Name perfil.Followers
+    printfn "%s" perfil.Bio
+    printfn "Blog: %s" perfil.Blog
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
