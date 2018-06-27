b1765c0016bf0790165e4014ca1b668b4c4d5845
Gabriel Schade <gabriel.schade@hotmail.com>
2018-06-27 10:03:50 -0300

    Postagem sobre o World Bank Type Provider e ajuste na pasta do JSON Type Provider

---
+++ WorldBankTypeProviderSample/WorldBankTypeProviderSample/Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+open FSharp.Data
+
+[<EntryPoint>]
+let main argv =
+    let bancoDadosGlobal = WorldBankData.GetDataContext()
+    let paises = [|
+        bancoDadosGlobal.Countries.Brazil
+        bancoDadosGlobal.Countries.Canada
+        bancoDadosGlobal.Countries.Ireland
+        bancoDadosGlobal.Countries.``United Kingdom``
+        bancoDadosGlobal.Countries.``United States``
+        bancoDadosGlobal.Countries.China
+        bancoDadosGlobal.Countries.Japan
+    |]
+
+    printfn "País | Capital | População | Homens | Mulheres | 0-14 anos | 15-64 anos | acima"
+    paises
+    |> Array.iter ( fun pais -> 
+        printfn "%s | %s | %.0f | %.0f | %.0f | %.0f | %.0f | %.0f" 
+            pais.Name 
+            pais.CapitalCity
+            pais.Indicators.``Population, total``.[2015]
+            pais.Indicators.``Population, male``.[2015]
+            pais.Indicators.``Population, female``.[2015]
+            pais.Indicators.``Population ages 0-14, total``.[2015]
+            pais.Indicators.``Population ages 15-64, total``.[2015]
+            pais.Indicators.``Population ages 65 and above, total``.[2015]
+    )
+
+
+    Console.ReadKey() |> ignore
+    0
